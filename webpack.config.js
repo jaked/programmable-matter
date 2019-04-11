54723cdca80d8aec2032db9c201a2a7d5c4c05b4
Jake Donham <jake.donham@gmail.com>
2019-04-11 10:27:48 -0700

    add webpack-dev-server setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,28 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  context: path.join(__dirname, './'),
   entry: './app/app.jsx',
+
   output: {
-//    path: path.join(__dirname, 'public'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
+
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         loader: 'jsx-loader',
-        exclude: /node_modules/,
-        include: path.join(__dirname, 'app'),
+        include: path.resolve(__dirname, 'app'),
       },
     ],
   },
+
+  devServer: {
+    inline: false
+  }
 };
