f948886ae40b805f480bbdba820c44a2ffb4e728
Jake Donham <jake.donham@gmail.com>
2019-04-18 12:49:47 -0700

    add tweet embed

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { TwitterTweetEmbed } from 'react-twitter-embed';
+
 /* Import Components */
 import { HelloWorld } from './components/HelloWorld';
 import { Display } from './components/Display';
 import { Editor } from './components/Editor';
 
-ReactDOM.render(<Editor/>, document.getElementById('main'));
+const tweet = <TwitterTweetEmbed tweetId={'839303032403714048'} />
+
+ReactDOM.render(tweet, document.getElementById('main'));
