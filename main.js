152b0e510d06d85d3bfd6190a4c8144924e9e8ee
Jake Donham <jake.donham@gmail.com>
2019-04-19 09:47:34 -0700

    hook up react dev tools

--- main.js
+++ main.js
@@ -4,11 +4,17 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 require('electron-reload')(path.resolve(__dirname, 'build'));
 
+const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
+
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
