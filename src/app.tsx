a34d9fac1eb8318bae3c1559891c4d6231bced79
Jake Donham <jake.donham@gmail.com>
2019-04-19 11:00:45 -0700

    hook up tweet embeds

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,4 @@ import * as ReactDOM from 'react-dom';
 
 import { Main } from './components/Main';
 
-// import { TwitterTweetEmbed } from 'react-twitter-embed';
-// const tweet = <TwitterTweetEmbed tweetId={'839303032403714048'} />
-
 ReactDOM.render(<Main/>, document.getElementById('main'));
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import MDX from '@mdx-js/runtime';
 
+import { TwitterTweetEmbed } from 'react-twitter-embed';
+
+const components = {
+  Tweet: props => <TwitterTweetEmbed {...props} />
+}
+
 export class Display extends React.Component<any, any> {
   render() {
     return (
-      <MDX>{this.props.content}</MDX>
+      <MDX components={components}>{this.props.content}</MDX>
     );
   }
 }
