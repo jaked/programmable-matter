0c966039ea53d96a9e5ec800f128711c820bf00e
Jake Donham <jake.donham@gmail.com>
2019-04-22 15:56:29 -0700

    render React components directly from MDX AST

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import MDX from '@mdx-js/runtime';
+
+import unified from 'unified';
+import toMDAST from 'remark-parse';
+import remarkMdx from 'remark-mdx';
+import squeeze from 'remark-squeeze-paragraphs';
+import toMDXAST from '@mdx-js/mdx/md-ast-to-mdx-ast';
+import mdxAstToMdxHast from '@mdx-js/mdx/mdx-ast-to-mdx-hast';
+
+import { Node } from '../parse/mdxhast'
 
 import { TwitterTweetEmbed } from 'react-twitter-embed';
 
@@ -12,9 +20,42 @@ interface Props {
 }
 
 export class Display extends React.Component<Props, {}> {
+  parser =
+    unified()
+      .use(toMDAST)
+      .use(remarkMdx)
+      .use(squeeze)
+      .use(toMDXAST)
+      .use(mdxAstToMdxHast)
+
   render() {
-    return (
-      <MDX components={components}>{this.props.content}</MDX>
-    );
+    const ast = this.parser.runSync(this.parser.parse(this.props.content)) as Node
+    return renderFromAst(ast)
   }
+}
+
+function renderFromAst(ast: Node): React.ReactNode {
+  switch (ast.type) {
+    case "root":
+      return React.createElement(
+        'div',
+        {},
+        ...ast.children.map(renderFromAst)
+      );
+      break;
+    case "element":
+      return React.createElement(
+        ast.tagName,
+        ast.properties,
+        ...ast.children.map(renderFromAst)
+      );
+      break;
+    case "text":
+      // TODO(jaked) handle interpolation
+      return ast.value;
+      break;
+    case "jsx":
+      return null;
+      break;
+   }
 }
---
+++ src/parse/mdxhast.ts
@@ -0,0 +1,47 @@
+// See
+//   https://github.com/syntax-tree/unist
+//   https://github.com/syntax-tree/hast
+//   https://mdxjs.com/advanced/ast/#mdxhast
+
+export interface Point {
+  line: number;
+  column: number;
+  offset?: number;
+}
+
+export interface Position {
+  start: Point;
+  end: Point;
+  indent?: number
+}
+
+interface NodeImpl {
+  type: string;
+  position?: Position;
+}
+
+interface ParentImpl extends NodeImpl {
+  children: Array<Element | Text | Jsx>;
+}
+
+export interface Root extends ParentImpl {
+  type: "root";
+}
+
+export interface Element extends ParentImpl {
+  type: "element";
+  tagName: string;
+  properties: Object;
+}
+
+export interface Text extends NodeImpl {
+  type: "text";
+  value: string;
+}
+
+export interface Jsx extends NodeImpl {
+  type: "jsx";
+  value: string;
+}
+
+export type Node = Root | Element | Text | Jsx
