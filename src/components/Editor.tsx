a7e99ee14cf2177d5af2db1b2ce7547c26434c34
Jake Donham <jake.donham@gmail.com>
2019-06-19 21:31:28 -0700

    failed attempt to use react-window to improve editor performance

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+// import { FixedSizeList } from 'react-window';
 import RSCEditor from './react-simple-code-editor';
 
 import styled from 'styled-components';
@@ -177,20 +178,50 @@ function computeHighlight(content: string, compiledNote: data.Note) {
     computeSpans(parsed.ast, spans);
   });
 
-  const elements: Array<React.ReactNode> = [];
-  let last = 0;
+  // TODO(jaked) this could use some tests
+  const lineStartOffsets: Array<number> = [0];
+  for (let i = 0; i < content.length; i++) {
+    if (content.charAt(i) === "\n" || i === content.length - 1)
+      lineStartOffsets.push(i + 1);
+  }
+
+  const lines: Array<React.ReactNode> = [];
+  let lineNodes: Array<React.ReactNode> = [];
+  let line = 0;
+  let lineEnd = lineStartOffsets[1];
+  let lastOffset = 0;
+
+  function pushLine() {
+    lines.push(<div>{lineNodes}</div>);
+    lineNodes = [];
+    lastOffset = lineEnd;
+    line += 1;
+    lineEnd = lineStartOffsets[line + 1];
+  }
+
   for (let i = 0; i < spans.length; i++) {
     const span = spans[i];
-    if (last < span.start) {
-      elements.push(content.slice(last, span.start));
+    while (lastOffset < span.start) {
+      if (span.start < lineEnd) {
+        lineNodes.push(content.slice(lastOffset, span.start));
+        lastOffset = span.start
+      } else {
+        lineNodes.push(content.slice(lastOffset, lineEnd));
+        pushLine();
+      }
     }
     const Component = span.Component;
     const chunk = content.slice(span.start, span.end);
-    elements.push(<Component key={i}>{chunk}</Component>);
-    last = span.end;
+    lineNodes.push(<Component>{chunk}</Component>);
+    lastOffset = span.end;
   }
-  if (last < content.length) {
-    elements.push(content.slice(last, content.length));
+  if (lastOffset < content.length) {
+    while (lastOffset < content.length) {
+      lineNodes.push(content.slice(lastOffset, lineEnd));
+      pushLine();
+    }
+  } else {
+    pushLine();
   }
 
   // the <br/> here is essential:
@@ -201,9 +232,33 @@ function computeHighlight(content: string, compiledNote: data.Note) {
   // so the textarea is a line short and scrolls up
   // (so its text is out of sync with the pre)
   // thus we add an extra linebreak to the pre
-  elements.push(<br/>);
+  lines.push(<br/>);
 
-  return elements;
+  return lines;
+
+  // TODO(jaked)
+  // this doesn't work, I think because we need the textarea and pre
+  // elements to scroll together (using the scrollbar on the outer div),
+  // so we don't want the outer divs produced by react-window.
+  // but maybe there is some code we can borrow?
+
+  // TODO(jaked)
+  // also, fixed-height lines doesn't work with line wrapping
+  // but we could compute the wrapping (maybe?) and use VariableSizeList
+
+  // const Row = ({ index, style }: { index: number, style: any }) =>
+  //   <div style={style}>{lines[index]}</div>
+
+  // return (
+  //   <FixedSizeList
+  //     itemCount={lines.length}
+  //     itemSize={19} // TODO(jaked) compute line height
+  //     width='100%'
+  //     height={1400} // TODO(jaked) compute actual heigh
+  //   >
+  //     {Row}
+  //   </FixedSizeList>
+  // );
 }
 
 export class Editor extends React.Component<Props, {}> {
