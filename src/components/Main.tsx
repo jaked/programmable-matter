4828460bfd08755e0f55b7e7f3d1e1b0831b1fb8
Jake Donham <jake.donham@gmail.com>
2020-01-31 11:38:12 -0800

    break out Sidebar component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,8 +8,7 @@ import { App } from '../app';
 import { Catch } from './Catch';
 import { Display } from './Display';
 import { Editor } from './Editor';
-import { Notes } from './Notes';
-import { SearchBox } from './SearchBox';
+import { Sidebar } from './Sidebar';
 
 interface Props {
   app: App;
@@ -23,66 +22,27 @@ const Flex = styled(FlexBase)({
 }, borders);
 
 export class Main extends React.Component<Props, {}> {
-  notesRef = React.createRef<HTMLDivElement>();
+  sidebarRef = React.createRef<Sidebar>();
   editorRef = React.createRef<Editor>();
-  searchBoxRef = React.createRef<SearchBox>();
-
-  constructor(props: Props) {
-    super(props);
-  }
 
   focusSearchBox = () => {
-    this.searchBoxRef.current && this.searchBoxRef.current.focus();
-  }
-
-  onKeyDown = (e: React.KeyboardEvent): boolean => {
-    if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey)
-      return false;
-
-    switch (e.key) {
-      case 'ArrowUp':
-        this.notesRef.current && this.notesRef.current.focus();
-        this.props.app.setSelected(this.props.app.matchingNotes[this.props.app.matchingNotes.length - 1].tag);
-        return true;
-
-      case 'ArrowDown':
-        this.notesRef.current && this.notesRef.current.focus();
-        this.props.app.setSelected(this.props.app.matchingNotes[0].tag);
-        return true;
-
-      case 'Enter':
-        if (this.props.app.matchingNotes.every(note => note.tag !== this.props.app.search)) {
-          this.props.app.newNote(this.props.app.search);
-        }
-        this.props.app.setSelected(this.props.app.search);
-        if (this.editorRef.current) {
-          this.editorRef.current.focus();
-        }
-        return true;
-
-      default: return false;
-    }
+    this.sidebarRef.current && this.sidebarRef.current.focusSearchBox();
   }
 
   focusEditor = (): void => {
-    if (this.editorRef.current) {
-      this.editorRef.current.focus();
-    }
+    this.editorRef.current && this.editorRef.current.focus();
   }
 
-  SideBar = ({ width }: { width: number }) =>
+  SidebarPane = ({ width }: { width: number }) =>
     <Flex width={width} flexDirection='column'>
-      <SearchBox
-        ref={this.searchBoxRef}
+      <Sidebar
+        ref={this.sidebarRef}
         search={this.props.app.search}
         onSearch={this.props.app.setSearch}
-        onKeyDown={this.onKeyDown}
-      />
-      <Notes
-        ref={this.notesRef}
-        notes={this.props.app.matchingNotes}
+        matchingNotes={this.props.app.matchingNotes}
         selected={this.props.app.selected}
         onSelect={this.props.app.setSelected}
+        newNote={this.props.app.newNote}
         focusEditor={this.focusEditor}
       />
     </Flex>
@@ -136,7 +96,7 @@ export class Main extends React.Component<Props, {}> {
     return (
       <>
         <Flex style={{ height: '100vh' }}>
-          { sideBarWidth === 0 ? null : <this.SideBar width={sideBarWidth} /> }
+          { sideBarWidth === 0 ? null : <this.SidebarPane width={sideBarWidth} /> }
           { editorPaneWidth === 0 ? null : <this.EditorPane width={editorPaneWidth} /> }
           { displayPaneWidth === 0 ? null : <this.DisplayPane width={displayPaneWidth} /> }
         </Flex>
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Notes } from './Notes';
+import { SearchBox } from './SearchBox';
+import * as data from '../data';
+
+type Props = {
+  search: string;
+  onSearch: (s: string) => void;
+  matchingNotes: data.Note[];
+  selected: string | null;
+  onSelect: (s: string | null) => void;
+  newNote: (s: string) => void;
+  focusEditor: () => void;
+}
+
+export class Sidebar extends React.Component<Props, {}> {
+  notesRef = React.createRef<HTMLDivElement>();
+  searchBoxRef = React.createRef<SearchBox>();
+
+  focusSearchBox = () => {
+    this.searchBoxRef.current && this.searchBoxRef.current.focus();
+  }
+
+  focusNotes = () => {
+    this.notesRef.current && this.notesRef.current.focus();
+  }
+
+  onKeyDown = (e: React.KeyboardEvent): boolean => {
+    if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey)
+      return false;
+
+    switch (e.key) {
+      case 'ArrowUp':
+        this.focusNotes();
+        this.props.onSelect(this.props.matchingNotes[this.props.matchingNotes.length - 1].tag);
+        return true;
+
+      case 'ArrowDown':
+        this.focusNotes();
+        this.props.onSelect(this.props.matchingNotes[0].tag);
+        return true;
+
+      case 'Enter':
+        if (this.props.matchingNotes.every(note => note.tag !== this.props.search)) {
+          this.props.newNote(this.props.search);
+        }
+        this.props.onSelect(this.props.search);
+        this.props.focusEditor();
+        return true;
+
+      default: return false;
+    }
+  }
+
+  render() {
+    return (<>
+      <SearchBox
+        ref={this.searchBoxRef}
+        search={this.props.search}
+        onSearch={this.props.onSearch}
+        onKeyDown={this.onKeyDown}
+      />
+      <Notes
+        ref={this.notesRef}
+        notes={this.props.matchingNotes}
+        selected={this.props.selected}
+        onSelect={this.props.onSelect}
+        focusEditor={this.props.focusEditor}
+      />
+    </>);
+  }
+}
