e8eb47b61ade19179ca6f8ae129a4bf74487a68c
Jake Donham <jake.donham@gmail.com>
2020-04-28 17:11:09 -0700

    turns out images can be created from Buffers

--- src/lang/Compile/compileFileJpeg.ts
+++ src/lang/Compile/compileFileJpeg.ts
@@ -2,25 +2,24 @@ import * as React from 'react';
 import Signal from '../../util/Signal';
 import Trace from '../../util/Trace';
 import Try from '../../util/Try';
-import * as Tag from '../../util/Tag';
 import Type from '../Type';
 import * as data from '../../data';
 
 function compileJpeg(
-  tag: string,
+  buffer: Buffer,
 ): data.Compiled {
   // TODO(jaked) parse JPEG file and return metadata
   const exportType = Type.module({ });
   const exportValue = { };
 
   const rendered = Signal.ok(
-    // it doesn't seem to be straightforward to create an img node
-    // directly from JPEG data, so we serve it via the dev server
-    // TODO(jaked) plumb port from top-level
     React.createElement(
       'img',
       {
-        src: `http://localhost:3000/${tag}.jpeg`,
+        // TODO(jaked) these URLs need to be freed
+        // https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+        // maybe we can wrap the img in a component that frees on prop change
+        src: URL.createObjectURL(new Blob([buffer.buffer])),
         style: {
           maxWidth: '100%',
           objectFit: 'contain' // ???
@@ -35,9 +34,6 @@ export default function compileFileJpeg(
   trace: Trace,
   file: data.File
 ): Signal<data.CompiledFile> {
-  // TODO(jaked) this probably doesn't update when the file data is changed,
-  // since it's the same URL in the render tree
-  // maybe we need a cache-busting arg?
-  return file.bufferCell.map(_ => compileJpeg(Tag.tagOfPath(file.path)))
+  return file.bufferCell.map(buffer => compileJpeg(buffer))
     .map(compiled => ({ ...compiled, ast: Try.ok(null) }));
 }
