58263af31bf6c94506b19f43ef93233b2eca1bb0
Jake Donham <jake.donham@gmail.com>
2020-05-06 23:15:08 -0700

    use Record for Type values

--- src/lang/Type/constructors.ts
+++ src/lang/Type/constructors.ts
@@ -1,60 +1,110 @@
 import * as Immutable from 'immutable';
+import Record from '../../util/Record';
 import * as Types from './types';
 import * as Union from './union';
 
-export const never: Types.NeverType = { kind: 'never' };
-export const unknown: Types.UnknownType = { kind: 'unknown' };
-export const undefinedType: Types.UndefinedType = { kind: 'undefined' };
+class NeverType extends Record<NeverType> implements Types.NeverType {
+  kind: 'never' = 'never';
+}
+export const never: Types.NeverType = new NeverType();
+
+class UnknownType extends Record<UnknownType> implements Types.UnknownType {
+  kind: 'unknown' = 'unknown';
+}
+export const unknown: Types.UnknownType = new UnknownType();
+
+class UndefinedType extends Record<UndefinedType> implements Types.UndefinedType {
+  kind: 'undefined' = 'undefined';
+}
+export const undefinedType: Types.UndefinedType = new UndefinedType();
 export const undefined = undefinedType;
-export const nullType: Types.NullType = { kind: 'null' };
-export const booleanType: Types.BooleanType = { kind: 'boolean' };
+
+class NullType extends Record<NullType> implements Types.NullType {
+  kind: 'null' = 'null';
+}
+export const nullType: Types.NullType = new NullType();
+
+class BooleanType extends Record<BooleanType> implements Types.BooleanType {
+  kind: 'boolean' = 'boolean';
+}
+export const booleanType: Types.BooleanType = new BooleanType();
 export const boolean = booleanType;
-export const numberType: Types.NumberType = { kind: 'number' };
+
+class NumberType extends Record<NumberType> implements Types.NumberType {
+  kind: 'number' = 'number';
+}
+export const numberType: Types.NumberType = new NumberType();
 export const number = numberType;
-export const stringType: Types.StringType = { kind: 'string' };
+
+class StringType extends Record<StringType> implements Types.StringType {
+  kind: 'string' = 'string';
+}
+export const stringType: Types.StringType = new StringType();
 export const string = stringType;
 
+class TupleType extends Record<TupleType> implements Types.TupleType {
+  kind: 'Tuple' = 'Tuple';
+  elems: Immutable.List<Types.Type> = Immutable.List();
+}
 export function tuple(...elems: Array<Types.Type>): Types.TupleType {
-  return { kind: 'Tuple', elems: Immutable.List(elems) };
+  return new TupleType({ elems: Immutable.List(elems) });
 }
 
+class ArrayType extends Record<ArrayType> implements Types.ArrayType {
+  kind: 'Array' = 'Array';
+  elem: Types.Type = never;
+}
 export function array(elem: Types.Type): Types.ArrayType {
-  return { kind: 'Array', elem };
+  return new ArrayType({ elem });
 }
 
+class SetType extends Record<SetType> implements Types.SetType {
+  kind: 'Set' = 'Set';
+  elem: Types.Type = never;
+}
 export function set(elem: Types.Type): Types.SetType {
-  return { kind: 'Set', elem };
+  return new SetType({ elem });
 }
 
+class MapType extends Record<MapType> implements Types.MapType {
+  kind: 'Map' = 'Map';
+  key: Types.Type = never;
+  value: Types.Type = never;
+}
 export function map(key: Types.Type, value: Types.Type): Types.MapType {
-  return { kind: 'Map', key, value };
+  return new MapType({ key, value });
 }
 
+class AbstractType extends Record<AbstractType> implements Types.AbstractType {
+  kind: 'Abstract' = 'Abstract';
+  label: string = '';
+  params: Immutable.List<Types.Type> = Immutable.List();
+}
 export function abstract(label: string, ...params: Array<Types.Type>): Types.AbstractType {
-  return { kind: 'Abstract', label, params: Immutable.List(params) };
+  return new AbstractType({ label, params: Immutable.List(params) });
 }
 
+class FunctionType extends Record<FunctionType> implements Types.FunctionType {
+  kind: 'Function' = 'Function';
+  args: Immutable.List<Types.Type> = Immutable.List();
+  ret: Types.Type = never;
+}
 export function functionType(
   args: Array<Types.Type>,
   ret: Types.Type
 ): Types.FunctionType {
-  return { kind: 'Function', args: Immutable.List(args), ret };
+  return new FunctionType({ args: Immutable.List(args), ret });
 }
 
-class ObjectType implements Types.ObjectType {
+class ObjectType extends Record<ObjectType> implements Types.ObjectType {
   kind: 'Object' = 'Object';
-  fields: Immutable.List<{ field: string, type: Types.Type }>;
-
-  constructor(fields: Immutable.List<{ field: string, type: Types.Type }>) {
-    this.fields = fields;
-  }
+  fields: Immutable.List<{ field: string, type: Types.Type }> = Immutable.List();
 
   get(field: string) {
     const ft = this.fields.find(ft => ft.field === field);
     if (ft) return ft.type;
   }
 }
-
 export function object(
   fields:
     { [f: string]: Types.Type } |
@@ -62,42 +112,40 @@ export function object(
     Immutable.List<{ field: string, type: Types.Type }>
 ): Types.ObjectType {
   if (Immutable.List.isList(fields)) {
-    return new ObjectType(fields);
+    return new ObjectType({ fields });
   } else if (Array.isArray(fields)) {
-    return new ObjectType(Immutable.List(fields));
+    return object(Immutable.List(fields));
   } else {
-    return new ObjectType(
-      Immutable.List(
-        Object.entries(fields).map(([ field, type]) => ({ field, type }))
-      )
-    );
+    return object(Object.entries(fields).map(([ field, type]) => ({ field, type })));
   }
 }
 
-class ModuleType implements Types.ModuleType {
+class ModuleType extends Record<ModuleType> implements Types.ModuleType {
   kind: 'Module' = 'Module';
-  fields: Immutable.List<{ field: string, type: Types.Type }>;
-
-  constructor(fields: Array<{ field: string, type: Types.Type }>) {
-    this.fields = Immutable.List(fields);
-  }
+  fields: Immutable.List<{ field: string, type: Types.Type }> = Immutable.List();
 
   get(field: string) {
     const ft = this.fields.find(ft => ft.field === field);
     if (ft) return ft.type;
   }
 }
-
 export function module(obj: { [f: string]: Types.Type }): Types.ModuleType {
-  return new ModuleType(Object.entries(obj).map(([ field, type ]) => ({ field, type })));
+  return new ModuleType({
+    fields: Immutable.List(Object.entries(obj).map(([ field, type ]) => ({ field, type })))
+  });
 }
 
+class SingletonType extends Record<SingletonType> implements Types.SingletonType {
+  kind: 'Singleton' = 'Singleton';
+  base: Types.Type = never;
+  value: any = undefined;
+}
 export function singleton(value: any): Types.Type {
   const type = typeof value;
   switch (type) {
-    case 'boolean': return { kind: 'Singleton', base: booleanType, value };
-    case 'number': return { kind: 'Singleton', base: numberType, value };
-    case 'string': return { kind: 'Singleton', base: stringType, value };
+    case 'boolean': return new SingletonType({ base: booleanType, value });
+    case 'number': return new SingletonType({ base: numberType, value });
+    case 'string': return new SingletonType({ base: stringType, value });
     case 'undefined': return undefinedType;
     case 'object':
       if (value === null) return nullType;
@@ -108,19 +156,31 @@ export function singleton(value: any): Types.Type {
   }
 }
 
+class NotType extends Record<NotType> implements Types.NotType {
+  kind: 'Not' = 'Not';
+  type: Types.Type = never;
+}
 export function not(type: Types.Type): Types.Type {
   if (type.kind === 'Not') return type.type;
-  else return { kind: 'Not', type };
+  else return new NotType({ type });
 }
 
+class UnionType extends Record<UnionType> implements Types.UnionType {
+  kind: 'Union' = 'Union';
+  types: Immutable.List<Types.Type> = Immutable.List();
+}
 // assumes that `types` satisfy the union invariants
 export function union(...types: Array<Types.Type>): Types.UnionType {
-  return { kind: 'Union', types: Immutable.List(types) };
+  return new UnionType({ types: Immutable.List(types) });
 }
 
+class IntersectionType extends Record<IntersectionType> implements Types.IntersectionType {
+  kind: 'Intersection' = 'Intersection';
+  types: Immutable.List<Types.Type> = Immutable.List();
+}
 // assumes that `types` satisfy the intersection invariants
 export function intersection(...types: Array<Types.Type>): Types.IntersectionType {
-  return { kind: 'Intersection', types: Immutable.List(types) };
+  return new IntersectionType({ types: Immutable.List(types) });
 }
 
 export function undefinedOr(t: Types.Type): Types.Type {
