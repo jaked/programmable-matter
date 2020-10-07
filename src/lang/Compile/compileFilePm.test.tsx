0a36be77c07315f773a86e9664f7fd2b05a9b46e
Jake Donham <jake.donham@gmail.com>
2020-10-06 20:01:40 -0700

    give React keys to tree rendered from .pm

--- src/lang/Compile/compileFilePm.test.tsx
+++ src/lang/Compile/compileFilePm.test.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import TestRenderer from 'react-test-renderer';
 import * as PMAST from '../../PMAST';
 import File from '../../files/File';
 
 import compileFilePm from './compileFilePm';
+
+function expectRenderEqual(
+  a: React.ReactNode, // TODO(jaked) fix in `rendered` API
+  b: React.ReactElement,
+) {
+  const aRendered = TestRenderer.create(a as React.ReactElement).toJSON();
+  const bRendered = TestRenderer.create(b).toJSON();
+  expect(aRendered).toEqual(bRendered);
+}
 
 describe('compileFilePm', () => {
   it('compiles', () => {
@@ -23,9 +33,10 @@ describe('compileFilePm', () => {
     expect(compiled.get().problems).toBeFalsy();
 
     compiled.get().rendered.reconcile();
-    expect(compiled.get().rendered.get()).toEqual([
-      <p>foo</p>
-    ]);
+    expectRenderEqual(
+      compiled.get().rendered.get(),
+      <p><span>foo</span></p>,
+    );
   });
 
   it('renders marks', () => {
@@ -49,14 +60,15 @@ describe('compileFilePm', () => {
     expect(compiled.get().problems).toBeFalsy();
 
     compiled.get().rendered.reconcile();
-    expect(compiled.get().rendered.get()).toEqual([
+    expectRenderEqual(
+      compiled.get().rendered.get(),
       <p>
-        foo
-        <strong>bar</strong>
-        <u>baz</u>
-        <em><strong>quux</strong></em>
+        <span>foo</span>
+        <span><strong>bar</strong></span>
+        <span><u>baz</u></span>
+        <span><em><strong>quux</strong></em></span>
       </p>
-    ]);
+    );
   });
 
   it('renders headers', () => {
@@ -74,10 +86,13 @@ describe('compileFilePm', () => {
     expect(compiled.get().problems).toBeFalsy();
 
     compiled.get().rendered.reconcile();
-    expect(compiled.get().rendered.get()).toEqual([
-      <p>foo</p>,
-      <h1>bar</h1>,
-      <h2><strong>baz</strong></h2>,
-    ]);
+    expectRenderEqual(
+      compiled.get().rendered.get(),
+      <>
+        <p><span>foo</span></p>
+        <h1><span>bar</span></h1>
+        <h2><span><strong>baz</strong></span></h2>
+      </>
+    );
   });
 });
--- src/lang/Compile/compileFilePm.tsx
+++ src/lang/Compile/compileFilePm.tsx
@@ -7,27 +7,29 @@ import * as data from '../../data';
 import * as PMAST from '../../PMAST';
 import Type from '../Type';
 
+let nextKey = 0;
+const KEYS = new WeakMap<PMAST.Node, string>();
+function findKey(node: PMAST.Node): string {
+  let key = KEYS.get(node);
+  if (key === undefined) {
+    key = `${nextKey++}`;
+    KEYS.set(node, key);
+  }
+  return key;
+}
+
 export const renderNode = (node: PMAST.Node) => {
+  const key = findKey(node);
   if ('text' in node) {
     let text: any = node.text;
     if (node.bold)      text = <strong>{text}</strong>;
     if (node.italic)    text = <em>{text}</em>;
     if (node.underline) text = <u>{text}</u>;
     if (node.code)      text = <code>{text}</code>;
-    return text;
+    return <span key={key}>{text}</span>;
   } else {
-    let children = node.children.map(renderNode);
-    // makes writing tests with JSX easier
-    if (children.length === 1) children = children[0];
-    switch (node.type) {
-      case 'p':   return <p>{children}</p>;
-      case 'h1':  return <h1>{children}</h1>;
-      case 'h2':  return <h2>{children}</h2>;
-      case 'h3':  return <h3>{children}</h3>;
-      case 'h4':  return <h4>{children}</h4>;
-      case 'h5':  return <h5>{children}</h5>;
-      case 'h6':  return <h6>{children}</h6>;
-    }
+    const children = node.children.map(renderNode);
+    return React.createElement(node.type, { key }, ...children);
   }
 }
 
