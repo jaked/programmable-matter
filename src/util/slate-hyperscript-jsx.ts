38af44f4a6962cffa5a261e7f2c1ae340a2f0dee
Jake Donham <jake.donham@gmail.com>
2020-10-22 11:20:51 -0700

    use slate-hyperscript for tests

---
+++ src/util/slate-hyperscript-jsx.ts
@@ -0,0 +1,32 @@
+import { createHyperscript } from 'slate-hyperscript';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      editor: any;
+      anchor: any;
+      focus: any;
+      cursor: any;
+
+      // TODO(jaked)
+      // we can't override the React `text` declaration
+      // and I can't figure out how to omit it
+      // so we use an alternate name
+      stext: { children?: any, bold?: boolean };
+
+    }
+  }
+}
+
+export const jsx = createHyperscript({
+  elements: {
+    'p': { type: 'p' },
+    'h1': { type: 'h1' },
+    'li': { type: 'li' },
+    'ol': { type: 'ol' },
+    'ul': { type: 'ul' },
+  },
+  creators: {
+    'stext': (tag, attrs, ...children) => jsx('text', attrs, ...children)
+  }
+});
